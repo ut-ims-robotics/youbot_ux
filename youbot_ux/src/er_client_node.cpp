1a44413940c674a503fa7095a3ebc1fe35e6879b
Juku80001 <hendrikolesk@gmail.com>
2021-02-25 22:20:37 +0200

    temoto nodes load/unload, WIP trajectory recording controls

--- youbot_ux/src/er_client_node.cpp
+++ youbot_ux/src/er_client_node.cpp
@@ -27,50 +27,35 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::Subscriber stateSub = nh.subscribe("state", 1, stateCallback);
 
-  /*
-   * Load a ROS program an example of a ROS executable (regularly invoked via 'rosrun'). The first
-   * parameter indicates the ROS package name and the second indicates the executable. Additional
-   * arguments can also be passed as a third std::string variable. The same method can be used to
-   * load ROS launch files
-   */
-  ROS_INFO("Load youbot_drive_joy");
-  ermi.loadRosResource("youbot_ux", "youbot_drive_joy.py");
-  //temoto_er_manager::LoadExtResource load_resource_msg_drive = ermi.loadRosResource("youbot_ux", "youbot_drive_joy");
-  ROS_INFO("Load velocity_move_youbot");
+  // load all required nodes into variables for later use
+  temoto_er_manager::LoadExtResource load_resource_msg_drive = ermi.loadRosResource("youbot_ux", "youbot_drive_joy.py");
   temoto_er_manager::LoadExtResource load_resource_msg_velocity = ermi.loadRosResource("youbot_ux", "velocity_move_youbot.py");
-  ROS_INFO("Load velocity_move_youbot_grasp");
   temoto_er_manager::LoadExtResource load_resource_msg_velocity_grasp = ermi.loadRosResource("youbot_ux", "velocity_move_youbot_grasp.py");
-  ROS_INFO("Load youbot_trajectory_record");
-  temoto_er_manager::LoadExtResource load_resource_msg_trajectory_record = ermi.loadRosResource("youbot_ux", "youbot_trajectory_record.py");
+  //temoto_er_manager::LoadExtResource load_resource_msg_trajectory_record = ermi.loadRosResource("youbot_ux", "youbot_trajectory_record.py");
 
-  /*
-  if (currentState != lastState) {
-    if (currentState == "Driving") {
-      ermi.loadRosResource("youbot_ux", "youbot_drive_joy");
-      ermi.unloadResource(load_resource_msg_velocity);
-      ermi.unloadResource(load_resource_msg_velocity_grasp);
-      ermi.unloadResource(load_resource_msg_trajectory_record);
+  while (ros::ok()) {
+    if (currentState != lastState) {
+      if (currentState == "Driving") {
+        temoto_er_manager::LoadExtResource load_resource_msg_drive = ermi.loadRosResource("youbot_ux", "youbot_drive_joy.py");
+        ermi.unloadResource(load_resource_msg_velocity);
+        ermi.unloadResource(load_resource_msg_velocity_grasp);
+        //ermi.unloadResource(load_resource_msg_trajectory_record);
+      }
+      else if (currentState == "ManipulatorPerJoint") {
+        temoto_er_manager::LoadExtResource load_resource_msg_velocity = ermi.loadRosResource("youbot_ux", "velocity_move_youbot.py");
+        temoto_er_manager::LoadExtResource load_resource_msg_velocity_grasp = ermi.loadRosResource("youbot_ux", "velocity_move_youbot_grasp.py");
+        ermi.unloadResource(load_resource_msg_drive);
+        //ermi.unloadResource(load_resource_msg_trajectory_record);
+      }
+      else if (currentState == "TrajectoryRecord") {
+        //temoto_er_manager::LoadExtResource load_resource_msg_trajectory_record = ermi.loadRosResource("youbot_ux", "youbot_trajectory_record.py");
+        ermi.unloadResource(load_resource_msg_drive);
+        ermi.unloadResource(load_resource_msg_velocity);
+        //ermi.unloadResource(load_resource_msg_velocity_grasp);
+      }
+      lastState = currentState;
     }
-    else if (currentState == "ManipulatorPerJoint") {
-      ermi.loadRosResource("youbot_ux", "velocity_move_youbot");
-      ermi.loadRosResource("youbot_ux", "velocity_move_youbot_grasp");
-      ermi.unloadResource(load_resource_msg_drive);
-      ermi.unloadResource(load_resource_msg_trajectory_record);
-    }
-    else if (currentState == "TrajectoryRecord") {
-      ermi.loadRosResource("youbot_ux", "youbot_trajectory_record");
-      ermi.unloadResource(load_resource_msg_drive);
-      ermi.unloadResource(load_resource_msg_velocity);
-      ermi.unloadResource(load_resource_msg_velocity_grasp);
-    }
-    lastState = currentState;
   }
-
-  */
-  /*
-   * Note that this time the "unloadResource" was not invoked, as the destructor of "ermi" automatically
-   * unloads all loaded resources.
-   */ 
-  while (true);
+  
   return 0;
 }
--- youbot_ux/src/youbot_trajectory_record.py
+++ youbot_ux/src/youbot_trajectory_record.py
@@ -5,24 +5,36 @@ import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String, Empty
 from std_srvs.srv import Empty
+from trajectory_replayer.srv import *
+from trajectory_recorder.srv import *
+
 import time
 
+# todo get services functioning (also in trajectory_replayer)
+# finish trajectory_record_state condition
+# make parameter server for controls (also add reading of such parameters to gamepad inputs in nodes)
+# figure out engine on off service
+# variable names recheck in all nodes
+# ...
 
 class TrajectoryRecordControl:
 	def __init__(self):
 		self.trajectory_record_state_pub = rospy.Publisher('trajectory_record_state', String, queue_size=1)
 		self.state_sub = rospy.Subscriber(rospy.get_param('~sub_topic', "state"), String, self.getState)
-		self.joy_sub = rospy.Subscriber(rospy.get_param('~sub_topic', "joy"), Joy, self.publish_cmd_velocities)
+		self.joy_sub = rospy.Subscriber(rospy.get_param('~sub_topic', "joy"), Joy, self.sub_joy_states)
 		self.trajectory_record_state = "idle"
 		self.stateMessage = "safemode"
 
 		self.motorsOff = rospy.ServiceProxy('arm_1/switchOffMotors', Empty)
 		self.motorsOn = rospy.ServiceProxy('arm_1/switchOnMotors', Empty) # how to get empty messages in rospy GLHF
 		
-		self.trajectoryRecordControlStart = rospy.ServiceProxy('control_trajectory_recorder', "recorder_action: 0")
-		self.trajectoryRecordControlStop = rospy.ServiceProxy('control_trajectory_recorder', "recorder_action: 1")
+		self.trajectoryRecordControl = rospy.ServiceProxy('control_trajectory_recorder', TrajectoryRecorderControl)
 		#self.trajectoryRecordGetRecorded = rospy.ServiceProxy('arm_1/switchOnMotors', "{}")
 		
+		self.recordControlButtonState = 0
+		self.playbackControlButtonState = 0
+		self.motorControlButtonState = 0
+
 		# Give the publishers time to get setup before trying to do any actual work.
         	rospy.sleep(2)
 	
@@ -30,39 +42,47 @@ class TrajectoryRecordControl:
 		self.stateMessage = string.data
 		#print(self.stateMessage)
 
-	def publish_cmd_velocities(self, joy):
+        def sub_joy_states(self, joy):
+		self.recordControlButtonState = joy.buttons[0]
+		self.playbackControlButtonState = joy.buttons[1]
+		self.motorControlButtonState = joy.buttons[3]
+
+	def trajectoryRecordControl(self, joy):
 		if self.stateMessage == "trajectoryRecord":
+
 			if self.trajectory_record_state == "idle":
-				if joy.buttons[0]:
+				if jself.recordControlButtonState:
 					self.trajectory_record_state = "record"
 					self.trajectory_record_state_pub("record")
-					self.trajectoryRecordControlStart()
-				elif joy.buttons[1]:
+					self.trajectoryRecordControl("recorder_action: 0")
+				elif self.playbackControlButtonState:
 					self.trajectory_record_state = "playback"
 					self.trajectory_record_state_pub("playback")
+
 			elif self.trajectory_record_state == "record":
-				if joy.buttons[0]:
+				if self.recordControlButtonState:
 					self.trajectory_record_state = "idle"
 					self.trajectory_record_state_pub("idle")
-					self.trajectoryRecordControlStop()
-				if joy.buttons[3]:
+					self.trajectoryRecordControl("recorder_action: 1")
+				if self.motorControlButtonState:
 					self.motorsOff()
 				else:
 					self.motorsOn()
+
 			elif self.trajectory_record_state == "playback":
 				print("playback")
-				
-		
-	def publish_cmd_velocities_stop(self):
-		self.cmd_vel_pub.publish(vel_cmd)
+				self.trajectory_record_state = "idle"
 
 	def main(self):
 		rate = rospy.Rate(10)
+		rospy.wait_for_service('control_trajectory_recorder')
+		rospy.wait_for_service('get_recorded_trajectory')
+		rospy.wait_for_service('trajectory_playback')
 		while not rospy.is_shutdown():
-			self.publish_arm_joint_velocities()
+			self.trajectoryRecordControl()
 			rate.sleep()
 		
 
 rospy.init_node('trajectory_record_control', anonymous=True)
-trajectoryRecordControl = TrajectoryRecordControl()
-trajectoryRecordControl.main()
+trajectoryRecord = TrajectoryRecordControl()
+trajectoryRecord.main()
