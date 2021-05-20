0242f986b4e96bff379c2bcca5718449955ee49d
Hendrik Olesk <hendrikolesk@gmail.com>
2021-05-20 03:38:19 +0300

    Update youbot_drive_joy.py

--- youbot_ux/scripts/youbot_drive_joy.py
+++ youbot_ux/scripts/youbot_drive_joy.py
@@ -7,13 +7,14 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import String
 import time
 
+# For controlling the youBot base
 
 class YoubotDrive:
 	def __init__(self):
-		self.cmdVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-		self.stateSub = rospy.Subscriber("state", String, self.getState)
-		self.joySub = rospy.Subscriber(rospy.get_param('~/control_options/controls/youbot_drive/controlsSubTopic', "joy"), Joy, self.subJoyStates)
-		self.stateMessage = "safemode"
+		self.cmdVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1) # Send base speed values
+		self.stateSub = rospy.Subscriber("state", String, self.getState)  # Receive current regime 
+		self.joySub = rospy.Subscriber(rospy.get_param('~/control_options/controls/youbot_drive/controlsSubTopic', "joy"), Joy, self.subJoyStates) # Get controller input
+		self.stateMessage = "safemode" # default known regime
 		
 		self.axisSpeedMultiplier = rospy.get_param('~/control_options/controls/youbot_drive/axisSpeedMultiplier', 0.5)
 		self.xAxis = 0.0
@@ -21,29 +22,25 @@ class YoubotDrive:
 		self.zAxis = 0.0
 
 		self.rate = rospy.Rate(20)
-		rospy.on_shutdown(self.publishCmdVelocitiesStop)
+		rospy.on_shutdown(self.publishCmdVelocitiesStop) # On node shutdown, send "0" values to stop the robot
 	
 	def getState(self, string):
 		self.stateMessage = string.data
-		#print(self.stateMessage)
 
 	def subJoyStates(self, joy):
 		self.xAxis = joy.axes[rospy.get_param('~/control_options/controls/youbot_drive/xAxes', 1)]
 		self.yAxis = joy.axes[rospy.get_param('~/control_options/controls/youbot_drive/yAxes', 0)]
 		self.zAxis = joy.axes[rospy.get_param('~/control_options/controls/youbot_drive/zAxes', 2)]
 
-
 	def publishCmdVelocities(self):
 		if self.stateMessage == "driving":
-			print("here")
 			velCmd = Twist()
-			velCmd.linear.x = self.xAxis*self.axisSpeedMultiplier
+			velCmd.linear.x = self.xAxis*self.axisSpeedMultiplier # Axis values 0.0 to 1.0 multiplied by preset value to decrease maximum speed
 			velCmd.linear.y = self.yAxis*self.axisSpeedMultiplier
 			velCmd.linear.z = 0
 			velCmd.angular.x = 0
 			velCmd.angular.y = 0
 			velCmd.angular.z = self.zAxis*self.axisSpeedMultiplier
-			print(self.xAxis)
 			self.cmdVelPub.publish(velCmd)
 		
 	def publishCmdVelocitiesStop(self):
