6122bccd550935d61cd63602f2c83ffa70261c55
Hendrik Olesk <hendrikolesk@gmail.com>
2021-05-20 03:49:53 +0300

    Update youbot_velocity_move.py

--- youbot_ux/scripts/youbot_velocity_move.py
+++ youbot_ux/scripts/youbot_velocity_move.py
@@ -10,10 +10,12 @@ from brics_actuator.msg import JointValue
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String, Int8
 
+# For sending youBot's manipulator speeds at which to move its joints
+
 class YoubotArm:
 	def __init__(self):
 		self.armJointPub = rospy.Publisher("arm_1/arm_controller/velocity_command", JointVelocities, queue_size=0)
-		self.armJointSelectPub = rospy.Publisher('joint_select', Int8, queue_size=1)
+		self.armJointSelectPub = rospy.Publisher('joint_select', Int8, queue_size=1) # Publish currently selected joint for feedback vibration on selection
 		self.stateSub = rospy.Subscriber(rospy.get_param('~state_sub_topic', "state"), String, self.getState)
 		self.joySub = rospy.Subscriber(rospy.get_param('~/control_options/controls/youbot_velocity_move/controlsSubTopic', "joy"), Joy, self.subJoyStates)
 		self.stateMessage = "safemode"
@@ -28,17 +30,17 @@ class YoubotArm:
 		self.jointAxisButtonX1 = 0.0
 		self.jointAxisButtonX2 = 0.0
 		self.jointAxisButtonY1 = 0.0
-		self.jointAxisButtonY2 = 0.0	
+		self.jointAxisButtonY2 = 0.0
+		
 
 		self.rate = rospy.Rate(10)
 
-		rospy.on_shutdown(self.publishArmJointVelocitiesStop)
+		rospy.on_shutdown(self.publishArmJointVelocitiesStop) # On shutdown, last things to be sent are "0" speed values
 
-	def getState(self, string):
+	def getState(self, string): # get current regime
 		self.stateMessage = string.data
-		#print(self.stateMessage)
 
-	def subJoyStates(self, joy):
+	def subJoyStates(self, joy): # get controller input values
 		self.jointSelectorUp = joy.buttons[rospy.get_param('~/control_options/controls/youbot_velocity_move/buttonSelectorUp', 15)]
 		self.jointSelectorDown = joy.buttons[rospy.get_param('~/control_options/controls/youbot_velocity_move/buttonSelectorDown', 17)]
 		self.jointAxisButtonX1 = -joy.axes[rospy.get_param('~/control_options/controls/youbot_velocity_move/axesButtonX', 0)]
@@ -49,7 +51,7 @@ class YoubotArm:
 	def publishArmJointVelocities(self):
 		if self.stateMessage == "manipulatorPerJoint":
 
-			# block to look for join selector button presses and switching of controllable joint
+			# block to look for joint selector button presses and switching of controllable joint
 			if self.prevClick == 0 and (self.jointSelectorUp == 1 or self.jointSelectorDown == 1):
 				if self.jointSelectorUp:
 					self.currentSelected += 1
@@ -64,13 +66,8 @@ class YoubotArm:
 			if self.jointSelectorUp == 0 and self.jointSelectorDown == 0:
 				self.prevClick = 0
 
-			joint1 = 0.0
-			joint2 = 0.0
-			joint3 = 0.0
-			joint4 = 0.0
-			joint5 = 0.0
-
-			jointVelocities = [joint1, joint2, joint3, joint4, joint5]
+			# Block to set joint velocity values according to controller inputs
+			jointVelocities = [0.0, 0.0, 0.0, 0.0, 0.0] 
 			jointVelocities[self.currentSelected] = self.axisSpeedMultiplier*(self.jointAxisButtonY1)
 			jointVelocities[0] = self.axisSpeedMultiplier*(self.jointAxisButtonX1)
 			jointVelocities[3] = self.axisSpeedMultiplier*(self.jointAxisButtonY2)
@@ -79,42 +76,35 @@ class YoubotArm:
 			jointCommands = []
 
 			for i in range(5): # setting each joint with new velocity
-				joint = JointValue()
-				joint.joint_uri = "arm_joint_" + str(i+1)
-				joint.unit = "s^-1 rad"
-				joint.value = jointVelocities[i]
+				joints = JointValue()
+				joints.joint_uri = "arm_joint_" + str(i+1)
+				joints.unit = "s^-1 rad"
+				joints.value = jointVelocities[i]
 
-				joint.timeStamp = rospy.Time.now()
-				jointCommands.append(joint)
-				#print(i)
-				#print(jointVelocities[i])
+				joints.timeStamp = rospy.Time.now()
+				jointCommands.append(joints)
 
 			desiredVelocities = JointVelocities()
 			desiredVelocities.velocities = jointCommands
 			self.armJointPub.publish(desiredVelocities)
 
-			if self.prevSelected != self.currentSelected:
+			if self.prevSelected != self.currentSelected: # If previously selected joint was different from current, publish new for controller feedback
 				self.armJointSelectPub.publish(self.currentSelected)
 				self.prevSelected = self.currentSelected
 
 	def publishArmJointVelocitiesStop(self): # function for stopping all joints on node shutdown 
-		joint1 = 0.0
-		joint2 = 0.0
-		joint3 = 0.0
-		joint4 = 0.0
-		joint5 = 0.0
 
-		jointVelocities = [joint1, joint2, joint3, joint4, joint5]
+		jointVelocities = [0.0, 0.0, 0.0, 0.0, 0.0]
 		jointCommands = []
 
 		for i in range(5):
-			joint = JointValue()
-			joint.joint_uri = "arm_joint_" + str(i+1)
-			joint.unit = "s^-1 rad"
-			joint.value = jointVelocities[i]
+			joints = JointValue()
+			joints.joint_uri = "arm_joint_" + str(i+1)
+			joints.unit = "s^-1 rad"
+			joints.value = jointVelocities[i]
 
-			joint.timeStamp = rospy.Time.now()
-			jointCommands.append(joint)
+			joints.timeStamp = rospy.Time.now()
+			jointCommands.append(joints)
 
 		desiredVelocities = JointVelocities()
 		desiredVelocities.velocities = jointCommands
